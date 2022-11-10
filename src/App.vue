82f06304ab843151fd61d01ccf073fbc849319fb
mohib <bekaltimohiboss@gmail.com>
2022-11-10 20:41:17 +0100

    Posts EntertainmentApp

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <header>
+      <h3>Entertainment App</h3>
+      
+    </header>
+    <subreddits category="food"> </subreddits>
+    <subreddits category="sports"> </subreddits>
+    <subreddits category="space"> </subreddits>
+    <subreddits category="art"> </subreddits>
+    <subreddits category="movies"> </subreddits>
+    <subreddits category="funny"> </subreddits>
+
+  </div>
+</template>
+
+<script>
+import subreddits from './components/subreddits'
+
+export default {
+  name: 'app',
+  components: {
+    subreddits
+
+  }
+}
+</script>
+
+<style>
+  body{
+    font-family: arial;
+    color: #666666;
+    background: #f4f4f4;
+    margin:0;
+    padding:0;
+  }
+
+  a{
+    text-decoration: none;
+  }
+  header{
+    background: coral;
+    color:#ffffff;
+    padding:10px;
+    text-align: center;
+  }
+</style>
---
+++ src/components/subreddit.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="subreddit">
+        <a :href="makeUrl(item.data.permalink)" :style="makeImage(item.data.thumbnail)" target="_blank " class="thumbnail"></a>
+        <div class="details">
+            <a :href="makeUrl(item.data.permalink)" :title="item.data.title" target="_blank" class="title">
+            {{item.data .title | truncate}}
+            </a>
+            <br/>
+            <div class="status">
+                <i class="material-icons">thumbs_up_down</i>
+                {{item.data.score}}
+                 <i class="material-icons">forum</i>
+                {{item.data.nim_comments}}
+            </div>
+        </div>
+    </div >
+</template>
+<script>
+
+export default{
+    name:'subreddit',
+    props:['item'],
+    methods: {
+        makeImage: function(img){
+            return 'background-image: url('+img+')';
+        },
+        makeUrl: function(permalink){
+            return 'https://reddit.com/'+permalink;
+        }
+    },
+    filters:{
+        truncate:function(value){
+            var length=50;
+            if(value.length<= length){
+                return value;
+            }else{
+                return value.substring(0,length)+'...';
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+    .subreddit{
+        display: flex;
+        
+    }
+    .subreddit .thumbnail{
+        display: block;
+        flex: 0 0 65px;
+        height: 65px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        margin-right: 12px;
+        border-radius: 5px;
+    }
+    .subreddit .details .titel{
+        font-size: 15px;
+        margin-bottom: 5px;
+        color:steelblue;
+    }
+    .subreddit .status{
+        font-size: 12px;
+        margin-right: 4px;
+        margin-top: 7px;
+        display: inline-block;
+        color:#666666;
+    }
+        .subreddit .status i{
+            font-size: 10px;
+            margin-right: 2px;
+        }
+
+</style>
